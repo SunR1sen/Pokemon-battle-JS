f4693f74df9ae41801a4810dc10def6db1a65b2c
SunR1sen <sunr1sen.dev@gmail.com>
2020-09-14 13:47:13 +0300

    Added bundle

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+        ],
+    }
+};
